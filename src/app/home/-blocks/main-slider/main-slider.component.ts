7108025f0cdc5fed8c510d1d2e886a1eecb6915f
beqa <btsalani@emis.ge>
2019-03-26 12:53:13 +0400

    owl carousel

--- src/app/home/-blocks/main-slider/main-slider.component.ts
+++ src/app/home/-blocks/main-slider/main-slider.component.ts
@@ -1,35 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 
 
 @Component({
   selector: 'pb-main-slider',
   templateUrl: './main-slider.component.html',
-  styleUrls: ['./main-slider.component.css']
+  styleUrls: ['./main-slider.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class MainSliderComponent implements OnInit {
   customOptions: any = {
     loop: true,
-    mouseDrag: false,
+    mouseDrag: true,
     touchDrag: false,
     pullDrag: false,
     dots: false,
     navSpeed: 700,
     navText: ['', ''],
-    responsive: {
-      0: {
-        items: 1
-      },
-      400: {
-        items: 2
-      },
-      740: {
-        items: 3
-      },
-      940: {
-        items: 4
-      }
-    },
-    nav: true
+    items: 1,
+    nav: true,
+    navContainer: false
   };
 
   constructor() { }
